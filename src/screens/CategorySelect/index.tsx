a3142fa9ad5e37e4439f2bd71fd12dc98c3215ad
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-07-07 20:20:39 -0300

    Aula 'Estilização da Interface para Selecionar Categoria' (C2-M2-SM1)

---
+++ src/screens/CategorySelect/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { FlatList } from "react-native";
+import { categories } from "../../utils/categories";
+
+import { Button } from "../../components/Forms/Button";
+
+import {
+  Container,
+  Header,
+  Title,
+  Category,
+  Icon,
+  Name,
+  Separator,
+  Footer,
+} from './styles'
+
+interface ICategory {
+  key: string
+  name: string
+}
+
+interface ICategorySelectProps {
+  category: string
+  setCategory: (category: ICategory) => void
+  closeSelectCategory: () => void
+}
+
+export function CategorySelect({
+  category,
+  setCategory,
+  closeSelectCategory
+}: ICategorySelectProps) {
+  return (
+    <Container>
+      <Header>
+        <Title>Categoria</Title>
+      </Header>
+
+      <FlatList
+        data={categories}
+        style={{ flex: 1, width: '100%'}}
+        keyExtractor={item => item.key}
+        renderItem={({ item }) => (
+          <Category>
+            <Icon name={item.icon} />
+            <Name>{item.name}</Name>
+          </Category>
+        )}
+        ItemSeparatorComponent={() => <Separator />}
+      />
+
+      <Footer>
+        <Button title="Selecionar" />
+      </Footer>
+    </Container>
+  )
+}
