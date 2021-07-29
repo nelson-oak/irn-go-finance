672f6289a3a6f6cf4fe6f3b8a3ac119aeffcb582
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-07-29 10:14:57 -0300

    Aula 'Utilizando Mocks' (C5-M1-SM2)

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -2,6 +2,22 @@ import { renderHook, act } from '@testing-library/react-hooks'
 
 import { AuthProvider, useAuth } from './auth'
 
+jest.mock('expo-google-app-auth', () => {
+  return {
+    logInAsync() {
+      return {
+        type: 'success',
+        user: {
+          id: 'user-id',
+          email: 'user-email',
+          name: 'user-name',
+          photo: 'user-photo',
+        }
+      }
+    }
+  }
+})
+
 describe('Auth Hook', () => {
   it('should be able to sign in with Google account', async () => {
     const { result } = renderHook(
@@ -13,6 +29,6 @@ describe('Auth Hook', () => {
 
     await act(() => result.current.signInWithGoogle())
 
-    expect(result.current.user.id).toBeTruthy()
+    expect(result.current.user.email).toBe('user-email')
   })
 })
